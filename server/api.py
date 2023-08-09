51291e6ec4e26af96710141012fd1531584ca730
sen <476025379@qq.com>
2023-08-09 22:52:22 +0200

    port test

--- server/api.py
+++ server/api.py
@@ -263,4 +263,4 @@ def get_roles():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8080, threaded=True)
+    app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)
