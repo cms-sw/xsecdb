0a7dfddcf75d6e6a0290b32bab13d9b807a2cb4f
Senphy <32735814+Senphy@users.noreply.github.com>
2023-08-09 21:21:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from server.api import app
 
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0', port=5000, threaded=True)
+    app.run(debug=True, host='127.0.0.1', port=5000, threaded=True)
