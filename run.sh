bbfedb59a704cb27686083f5e4afd6bb5d86a20f
Dawei Fu <fudw@pku.edu.cn>
2024-07-12 01:02:09 +0800

    resolve npm dependency conflicts

--- run.sh
+++ run.sh
@@ -1,16 +1,18 @@
 # install frontend dependencies
-cd client && npm install && npm install --save history
+cd client
+npm install --legacy-peer-deps webpack-cli
+
 # compile frontend files
 npm run compile && cd ..
 
 # create logs directory if doesn't exist
 mkdir -p logs
+touch logs/error.log
+touch logs/access.log
 
-# run virtual env
-. ../venv/bin/activate
 # install pip dependencies
 pip install -r requirements.txt
-# run server
-python main.py &> out.log &
 
-deactivate
+# run server
+echo $PWD
+python main.py
